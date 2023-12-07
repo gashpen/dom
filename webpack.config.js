a07f2d625c0ab7e3d9eb43614c16239a24625691
gashpen <constantinent@mail.ru>
2023-12-07 12:31:18 +0300

    add_yml

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devtool: 'source-map',
   entry: './src/js/app.js',
   module: {
     rules: [
